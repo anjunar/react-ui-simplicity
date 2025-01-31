047f3c31b26e620e2ef071d3e0d1c377b7cd0f43
Patrick Bittner <anjunar@gmx.de>
2025-01-31 02:50:32 +0100

    bug fix

--- packages/library/src/components/inputs/button/Button.tsx
+++ packages/library/src/components/inputs/button/Button.tsx
@@ -5,32 +5,6 @@ import {FormModel} from "../../shared/Model";
 import LinkContainerObject from "../../../domain/container/LinkContainerObject";
 import LinkObject from "../../../domain/container/LinkObject";
 
-export function renderButtons(container: LinkContainerObject, force? : boolean) {
-
-    function type(link: LinkObject) {
-        switch (link.method) {
-            case "POST" : return "submit"
-            case "PUT" : return  "submit"
-            case "DELETE" : return "submit"
-            default : return "button"
-        }
-    }
-
-    return (
-        <Fragment>
-            {
-                Object.values(container || {})
-                    .filter(link => link.method !== "GET" && link.rel !== "validate")
-                    .map(link => (
-                        <Button style={{flex: 1}} key={link.rel} name={link.rel} type={type(link)} force={link.method === "DELETE" || force || link.linkType === "action"}>
-                            {link.title}
-                        </Button>
-                    ))
-            }
-        </Fragment>
-    )
-}
-
 function Button(properties : Button.Attributes) {
 
     const {children, type, force = false, ...rest} = properties
@@ -75,6 +49,33 @@ namespace Button {
     export interface Attributes extends ButtonHTMLAttributes<HTMLButtonElement> {
         force? : boolean
     }
+
+    export function renderButtons(container: LinkContainerObject, force? : boolean) {
+
+        function type(link: LinkObject) {
+            switch (link.method) {
+                case "POST" : return "submit"
+                case "PUT" : return  "submit"
+                case "DELETE" : return "submit"
+                default : return "button"
+            }
+        }
+
+        return (
+            <Fragment>
+                {
+                    Object.values(container || {})
+                        .filter(link => link.method !== "GET" && link.rel !== "validate")
+                        .map(link => (
+                            <Button style={{flex: 1}} key={link.rel} name={link.rel} type={type(link)} force={link.method === "DELETE" || force || link.linkType === "action"}>
+                                {link.title}
+                            </Button>
+                        ))
+                }
+            </Fragment>
+        )
+    }
+
 }
 
 export default Button
